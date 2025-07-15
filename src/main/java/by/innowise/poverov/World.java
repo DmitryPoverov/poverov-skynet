5c7326e042a75bc3ef3c31e29a71d14a5e9a3947
DP <did.ink@mail.ru>
2025-07-15 13:45:20 +0300

    Wednesday.java, World.java - Create Wednesday and World factions subclasses extending Faction.

---
+++ src/main/java/by/innowise/poverov/World.java
@@ -0,0 +1,9 @@
+package by.innowise.poverov;
+
+public class World extends Faction {
+
+    public World(Storage storage, DayNightCycle dayNightCycle) {
+        super(storage, dayNightCycle);
+    }
+
+}
